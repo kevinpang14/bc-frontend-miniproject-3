fff17b0cb1dc913337ce4b43094688179d0adabb
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-10-25 17:56:58 +0700

    create class methods

--- main.js
+++ main.js
@@ -3,29 +3,79 @@
 
 //MAIN REQUIREMENTS - START
 class Developer {
-   
+  createPrototype() {}
 }
 
+//Front-End Developer START
 class FrontEndDeveloper extends Developer {
-    
+  createHTML() {}
+  createCSS() {}
+  createJavascript() {}
+  createUX() {}
 }
+
+//Front-End Developer END
+
+//Back-End Developer START
 
 class BackEndDeveloper extends Developer {
-    
+  createDb() {}
+  createServer() {}
+  createAPI() {}
+  manageSecurity() {}
 }
 
+//Back-End Developer END
+
+//Designer START
+class Designer {
+  createDesign() {}
+  createUI() {}
+}
+
+//Designer END
+
+//Project START
 class Project {
-
+  buildProject() {}
 }
 
-class Testing {
+//Project END
 
+//MAIN REQUIREMENTS - END
+
+//Project Manager START
+class ProjectManager {
+  manageProject(project) {}
+  assignTeam(project) {}
+  createProject(project) {}
+  createProjectPlan(project) {}
+  createProjectBudget(project) {}
 }
 
-class  DevOps {
+//Project Manager END
 
+//Software Tester START
+class SoftwareTester {
+  testProject(projectBeta) {}
 }
 
+//Software Tester END
+
+//DevOps START
+class DevOps {
+  deployProject(project) {}
+  monitorProject(project) {}
+}
+//DevOps END
+
+//Business Analyst START
 class BusinessAnalyst {
-
+  analyzeProject(project) {}
+  createRequirement(project) {}
+  createUserStory(project) {}
+  createBusinessProcess(project) {}
+  createBusinessCase(project) {}
+  createBusinessModel(project) {}
 }
+//Business Analyst END
