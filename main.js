09e0262a0c9f8370a5bf8e1ec9b80ba6e2882dca
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-10-25 16:33:38 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+//MINI PROJECT 3
+//UNDERSTANDING AND IMPLEMENTING SOLID PRINCIPLES
+
+//MAIN REQUIREMENTS - START
+class Developer {
+   
+}
+
+class FrontEndDeveloper extends Developer {
+    
+}
+
+class BackEndDeveloper extends Developer {
+    
+}
+
+class Project {
+
+}
+
+class Testing {
+
+}
+
+class  DevOps {
+
+}
+
+class BusinessAnalyst {
+
+}
